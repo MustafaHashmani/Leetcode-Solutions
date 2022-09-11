39dc9804eefde1a0bff08e6fa8dc4d5fd037daa2
Mustafa Hashmani <63756923+MustafaHashmani@users.noreply.github.com>
2022-09-12 01:18:23 +0500

    Time: 2 ms (73.16%), Space: 41.5 MB (37.63%) - LeetHub

---
+++ 258-add-digits/258-add-digits.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int addDigits(int num) {
+        if(num <= 9){
+            return num;
+        }
+        return addDigits(num%10 + addDigits(num/10)); 
+        
+    }
+}
